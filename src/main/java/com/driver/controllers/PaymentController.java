a997abf49d98851a7240353556684996910e72f8
svenkatesh0090 <venkateshcsp9@gmail.com>
2023-06-17 21:51:25 +0530

    Done

--- src/main/java/com/driver/controllers/PaymentController.java
+++ src/main/java/com/driver/controllers/PaymentController.java
@@ -14,7 +14,7 @@ public class PaymentController {
     PaymentServiceImpl paymentService;
 
     @PostMapping("/pay")
-    public Payment pay(@RequestParam Integer reservationId, @RequestParam Integer amountSent, @RequestParam String mode) throws Exception{
+    public Payment pay(@RequestParam Integer reservationId, @RequestParam Integer amountSent, @RequestParam String mode) throws Throwable{
         //Attempt a payment of amountSent for reservationId using the given mode ("cASh", "card", or "upi")
         //If the amountSent is less than bill, throw "Insufficient Amount" exception, otherwise update payment attributes
         //If the mode contains a string other than "cash", "card", or "upi" (any character in uppercase or lowercase), throw "Payment mode not detected" exception.
--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -16,7 +16,7 @@ public class ReservationController {
     @Autowired
     ReservationServiceImpl reservationService;
     @PostMapping("/reserveSpot")
-    public Reservation reserveSpot(@RequestParam Integer userId, @RequestParam Integer parkingLotId, @RequestParam Integer timeInHours, @RequestParam Integer numberOfWheels) throws Exception {
+    public Reservation reserveSpot(@RequestParam Integer userId, @RequestParam Integer parkingLotId, @RequestParam Integer timeInHours, @RequestParam Integer numberOfWheels) throws Throwable {
         //Reserve a spot in the given parkingLot such that the total price is minimum. Note that the price per hour for each spot is different
         //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
         //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
